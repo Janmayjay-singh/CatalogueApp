61e9033df371a76fee7c8575ce89ca89217ee69d
Janmayjay-singh <janmayjaysingh16@gmail.com>
2021-05-26 15:54:03 +0530

    Day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,17 @@
+class CatalogModel{
+static final items [
+  Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a"
+      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-max-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000")
+];
+}
+
 class Item{
-final String id;
+final int id;
 final String name;
 final String desc;
 final num price;
@@ -9,13 +21,3 @@ final String image;
 Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
 
-final products [Item(
-  id: "Codepur001",
-  name: "iPhone 12 Pro",
-  desc: "Apple iPhone 12th generation",
-  price: 999,
-  color: "#33505a"
-  image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-max-blue-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021658000"
-
-)
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:myfirst/models/catalog.dart';
 import 'package:myfirst/widgets/drawer.dart';
+import 'package:myfirst/widgets/item_widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
